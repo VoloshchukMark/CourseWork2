0bd091c2926849538734572d5b62699a607343d5
Mark Voloshchuk <77354267+Markovoloshchuk@users.noreply.github.com>
2025-11-24 19:42:34 +0200

    Make Model and Fabric classes / Prepare to make Create Info Windows
    Surely in future I'll change it a little. It's still just a sketch. But at least I know what to do next this way yaay

--- Materials/Fabric.py
+++ Materials/Fabric.py
@@ -1,1 +1,11 @@
-
+class Fabric:
+    def __init__(self):
+        self.fabric_name = None
+        self.fabric_manufacturer = None
+        self.fabric_color = None
+        self.texture = None
+        self.fabric_price_per_meter = 0.0
+        self.width_in_meters = 0.0
+        self.full_price = 0.0
+        self.image = None
+        self.in_stock = False
